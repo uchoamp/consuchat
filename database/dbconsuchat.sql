54e59d18c880c667198e3632e3f659b49812a28b
Marcos Pacheco <azurique10111@gmail.com>
2021-11-05 20:47:38 -0300

    atualizando database

--- database/dbconsuchat.sql
+++ database/dbconsuchat.sql
@@ -2,40 +2,40 @@ CREATE DATABASE IF NOT EXISTS dbconsuchat;
 
 USE dbconsuchat;
 
-CREATE TABLE cliente(
+CREATE TABLE IF NOT EXISTS cliente(
 	id INT AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
-    cpf CHAR(14) NOT NULL,
-    telefone CHAR(20) NOT NULL,
+    cpf CHAR(11) NOT NULL,
+    telefone CHAR(10) NOT NULL,
     endereco VARCHAR(255) NULL,
-    NASCIMENTO DATE NULL,
+    nascimento DATE NULL,
     PRIMARY KEY (id)
-) ENGINE=InnoDB;
+);
 
-CREATE TABLE clinica(
+CREATE TABLE IF NOT EXISTS clinica(
 	id INT AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
-	fone_contato CHAR(20) NOT NULL,
+	telfone CHAR(11) NOT NULL,
     tipo VARCHAR(255) NOT NULL,
     endereco VARCHAR(255) NOT NULL,
-    longitude VARCHAR(45) NOT NULL,
-    latitude VARCHAR(45) NOT NULL,
+    longitude VARCHAR(45),
+    latitude VARCHAR(45),
     PRIMARY KEY (id)
-) ENGINE=InnoDB;
+) ;
 
-CREATE TABLE consulta(
+CREATE TABLE IF NOT EXISTS consulta(
 	id INT AUTO_INCREMENT,
-    desc VARCHAR(255) NULL,
+    descricao VARCHAR(255) NULL,
     marcada DATETIME NOT NULL,
-    realizada BOOLEAN NULL,
+    realizada BOOLEAN,
     id_cliente INT NOT NULL,
     id_clinica INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (id_cliente) REFERENCES cliente(id),
-    FOREIGN KEY (id_clinica) REFERENCES clinica(id),
-) ENGINE=InnoDB;
+    FOREIGN KEY (id_clinica) REFERENCES clinica(id)
+);
 
-CREATE TABLE horario(
+CREATE TABLE IF NOT EXISTS horario(
     id INT AUTO_INCREMENT,
     am_inicio TIME NOT NULL,
     am_fim TIME,
@@ -43,17 +43,17 @@ CREATE TABLE horario(
     pm_fim TIME NOT NULL,
     intervalo TIME NOT NULL,
     almoco BOOLEAN,
-    dia_semana INT(1),
+    dia_semana TINYINT,
     id_clinica INT NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY (id_clinica) REFERENCES clinica(id),
-) ENGINE=InnoDB;
+    FOREIGN KEY (id_clinica) REFERENCES clinica(id)
+);
 
-CREATE TABLE funcionario(
+CREATE TABLE IF NOT EXISTS funcionario(
     id INT AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     id_clinica INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (id_clinica) REFERENCES clinica(id),
-) ENGINE=InnoDB;
+    FOREIGN KEY (id_clinica) REFERENCES clinica(id)
+);
