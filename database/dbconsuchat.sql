c8515e666541bbc3ca6a5ba7ee5c2d22d6dc2892
Marcos Pacheco <azurique10111@gmail.com>
2021-11-18 22:13:31 -0300

    id_table now is table_id

--- database/dbconsuchat.sql
+++ database/dbconsuchat.sql
@@ -3,19 +3,20 @@ CREATE DATABASE IF NOT EXISTS dbconsuchat;
 USE dbconsuchat;
 
 CREATE TABLE IF NOT EXISTS cliente(
-	id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
+    sobrenome VARCHAR(255) NOT NULL,
     cpf CHAR(11) NOT NULL,
-    telefone CHAR(10) NOT NULL,
+    telefone CHAR(11) NOT NULL,
     endereco VARCHAR(255) NULL,
     nascimento DATE NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS clinica(
-	id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
-	telfone CHAR(11) NOT NULL,
+    telefone CHAR(11) NOT NULL,
     tipo VARCHAR(255) NOT NULL,
     endereco VARCHAR(255) NOT NULL,
     longitude VARCHAR(45),
@@ -28,11 +29,11 @@ CREATE TABLE IF NOT EXISTS consulta(
     descricao VARCHAR(255) NULL,
     marcada DATETIME NOT NULL,
     realizada BOOLEAN,
-    id_cliente INT NOT NULL,
-    id_clinica INT NOT NULL,
+    cliente_id INT NOT NULL,
+    clinica_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (id_cliente) REFERENCES cliente(id),
-    FOREIGN KEY (id_clinica) REFERENCES clinica(id)
+    FOREIGN KEY (cliente_id) REFERENCES cliente(id),
+    FOREIGN KEY (clinica_id) REFERENCES clinica(id)
 );
 
 CREATE TABLE IF NOT EXISTS horario(
@@ -44,16 +45,19 @@ CREATE TABLE IF NOT EXISTS horario(
     intervalo TIME NOT NULL,
     almoco BOOLEAN,
     dia_semana TINYINT,
-    id_clinica INT NOT NULL,
+    clinica_id INT NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY (id_clinica) REFERENCES clinica(id)
+    FOREIGN KEY (clinica_id) REFERENCES clinica(id)
 );
 
 CREATE TABLE IF NOT EXISTS funcionario(
     id INT AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    sobrenome VARCHAR(255) NOT NULL, 
+    email VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    id_clinica INT NOT NULL,
+    clinica_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (id_clinica) REFERENCES clinica(id)
+    FOREIGN KEY (clinica_id) REFERENCES clinica(id)
 );
